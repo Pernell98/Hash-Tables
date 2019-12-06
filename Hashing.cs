643bbd94a30b6a139650f8b4c97a96c3ea8bdc8c
Pernell Robinson <probinso2017@students.curry.edu>
2019-12-06 17:43:27 -0500

    Design for the Hashing program

---
+++ Hashing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashing_Table
+{
+    class Hashing
+    {
+        public hashingTable insert()
+        {
+            //insert hashing table
+        }
+        
+
+        public hashTable remove()
+        {
+            //Remove hashtable
+        }
+
+
+
+        public hashTable find()
+        {
+            //Finds the hash tables
+        }
+
+    }
+}
